0e21cafc654f7c3b66dbbdffb0b9563820e55176
dsgnfox <dsgnfox@gmail.com>
2021-12-24 23:43:17 +0300

    stylish upd

--- formatter/stylish.js
+++ formatter/stylish.js
@@ -1,77 +1,49 @@
-const getChildren = (tree) => tree?.children;
+import _ from 'lodash';
 
-const getStatus = (tree) => tree?.status;
+const indent = ' '.repeat(4);
 
-const getStatusSymbol = (status) => {
-  const statuses = 
+const getIndent = (count) => indent.repeat(count);
 
-unchanged
-changed
-removed
-added
+const getStringifyTree = (lines, spaces) => (
+  [
+    '{',
+    ...lines,
+    `${spaces}}`,
+  ].join('\n')
+);
+
+const getValue = (data, depth) => {
+  if (!_.isObject(data)) {
+    return data;
+  }
+  const lines = Object
+    .entries(data)
+    .map(([key, value]) => {
+      const preparedValue = getValue(value, depth + 1);
+      return `${getIndent(depth)}${indent}${key}: ${preparedValue}`;
+    });
+  return getStringifyTree(lines, getIndent(depth));
 };
 
-const getName = (tree) => tree?.name;
+const getStringifyLine = (depth, sign, key, value) => `${getIndent(depth)} ${sign} ${key}: ${getValue(value, depth + 1)}`;
 
-const getNewValue = (tree) => tree?.newValue;
+const mapping = {
+  added: (depth, { name, newValue }) => getStringifyLine(depth, '+', name, newValue),
+  deleted: (depth, { name, oldValue }) => getStringifyLine(depth, '-', name, oldValue),
+  unchanged: (depth, { name, oldValue }) => getStringifyLine(depth, ' ', name, oldValue),
+  updated: (depth, { name, oldValue, newValue }) => [getStringifyLine(depth, '-', name, oldValue), getStringifyLine(depth, '+', name, newValue)],
+  nested: (depth, { name, children }, iter) => getStringifyLine(depth, ' ', name, iter(children, depth + 1)),
+};
 
-const getOldValue = (tree) => tree?.oldValue;
-
-const stylish = (tree) => {
-
-  const iter = (currentValue, depth) => {
-
-    const currentIndent = ' '.repeat(depth);
-    const children = getChildren(currentValue);
-    const name = getName(currentValue);
-
-    if (currentValue.isNode) {
-
-      const status = getStatus(currentValue);
-      const oldValue = getOldValue(currentValue);
-      const newValue = getNewValue(currentValue);
-
-      switch (status) {
-        case 'unchanged':
-          return `${currentIndent}  ${name}: ${oldValue}\n`;
-        case 'changed':
-          return `${currentIndent}- ${name}: ${oldValue}\n+ ${name}: ${newValue}\n`;
-        case 'removed':
-          return `${currentIndent}- ${name}: ${oldValue}\n`;
-        case 'added':
-          return `${currentIndent}+ ${name}: ${newValue}\n`;
-        default:
-          return false;
-      }
-    }
-
-    const bracketIndent = ' '.repeat(depth - 1);
-    const iterator = children ? children : currentValue;
-    const lines = `${currentIndent}${name}: {\n${iterator.flatMap(i => iter(i, depth += 1))}`;
-
-  
-    // console.log("lines", lines)
-    // const lines = `${name && name}: {${iter(children, depth += 1)}`;
-  
-
-    // // console.log('currentValue', currentValue)
-    // 
-    // const children = getChildren(currentValue);
-
-    // // console.log('name', name)
-    // // console.log('children', children)
-    // 
-
-    return [
-      lines,
-      `${bracketIndent}}`,
-    ].join('\n');
-
+const stylish = (data) => {
+  const iter = (tree, depth) => {
+    const lines = tree.flatMap((node) => {
+      // console.dir(node, {depth: null})
+      return mapping[node.status](depth, node, iter)
+    });
+    return getStringifyTree(lines, getIndent(depth));
   };
-
-
-  return iter(tree, 1);
-
+  return iter(data, 0);
 };
 
 export default stylish;
--- index.js
+++ index.js
@@ -2,39 +2,33 @@ import _ from 'lodash';
 import parser from './src/parsers.js';
 import stylish from './formatter/stylish.js';
 
-const getUniqKeys = (obj1, obj2) => _.uniq([...Object.keys(obj1), ...Object.keys(obj2)]);
+const getUniqSortedKeys = (obj1, obj2) => _.uniq([...Object.keys(obj1), ...Object.keys(obj2)]).sort();
 
+const makeDiffTree = (oldData = {}, newData = {}) => {
+  
+  const iter = (key, oldData, newData) => {
+    const oldValue = oldData[key];
+    const newValue = newData[key];
+    const status = getDiffStatus(key, oldData, newData);
+    const isNode = _.isObject(oldValue) === false || _.isObject(newValue) === false;
+  
+    if (isNode) {
+      return makeNode(key, oldValue, newValue, status);
+    }
 
-const makeDiffTree = (key, oldData = {}, newData = {}) => {
+    const uniqKeys = getUniqSortedKeys(oldValue, newValue);
+    return makeList(key, status, uniqKeys.map((uniqKey) => iter(uniqKey, oldValue, newValue)));
+  };
 
-  const oldValue = oldData[key];
-  const newValue = newData[key];
-  const status = getDiffStatus(key, oldData, newData);
+  const uniqKeys = getUniqSortedKeys(oldData, newData);
 
-  if (status === 'removed') {
-    const uniqKeys = Object.keys(oldValue) ?? [];
-    return _.isObject(oldValue) === false ? makeNode(key, oldValue, newValue, status) : makeList(key, status, uniqKeys.map((uniqKey) => makeDiffTree(uniqKey, oldValue)))
-  }
-
-  if (status === 'added') {
-    const uniqKeys = Object.keys(newValue) ?? [];
-    return _.isObject(newValue) === false ? makeNode(key, oldValue, newValue, status) : makeList(key, status, uniqKeys.map((uniqKey) => makeDiffTree(uniqKey, {}, newValue)))
-  }
-
-  if (_.isObject(oldValue) === false || _.isObject(newValue) === false) {
-    return makeNode(key, oldValue, newValue, status);
-  }
-
-  const uniqKeys = getUniqKeys(oldValue, newValue) ?? [];
-
-  return makeList(key, status, uniqKeys.map((uniqKey) => makeDiffTree(uniqKey, oldValue, newValue)))
+  return uniqKeys.map((key) => iter(key, oldData, newData));
 
 };
 
 const makeNode = (name, oldValue, newValue, status) => {
   return {
     name,
-    isNode: true,
     oldValue,
     newValue,
     status,
@@ -45,39 +39,27 @@ const makeList = (name, status, children = []) => {
   return {
     name,
     status,
-    isList: true,
     children: [...children],
   }
 };
 
-const getDiffStatus = (key, oldFile, newFile) => {
+const getDiffStatus = (key, before, after) => {
 
-  const oldValue = oldFile[key];
-  const newValue = newFile[key];
+  if (_.isPlainObject(before[key]) && _.isPlainObject(after[key])) return 'nested';
 
-    if (oldValue === newValue) {
-      return 'unchanged';
-    }
+  if (!_.has(before, key)) return 'added';
 
-    if (_.has(oldFile, key) && _.has(newFile, key)) {
-      return 'changed';
-    }
+  if (!_.has(after, key)) return 'deleted';
 
-    if (_.has(oldFile, key) && _.has(newFile, key) === false) {
-      return 'removed';
-    }
+  if (!_.isEqual(before[key], after[key])) return 'updated';
 
-    if (_.has(oldFile, key) === false && _.has(newFile, key)) {
-      return 'added'
-    }
+  return 'unchanged';
 }
 
 const genDiff = (oldFilePath, newFilePath) => {
   const oldFile = parser(oldFilePath);
   const newFile = parser(newFilePath);
-  const uniqKeys = getUniqKeys(oldFile, newFile) ?? [];
-  const diffs = uniqKeys.map((key) => makeDiffTree(key, oldFile, newFile));
-  const diffsWithRoot = makeList('root', '', diffs);
+  const diffs = makeDiffTree(oldFile, newFile);
   const styled = stylish(diffs);
   
   return styled;
